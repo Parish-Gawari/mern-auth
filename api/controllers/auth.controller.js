135fa1ad8ffb12008d0e913f3514cbda4e2dd019
Samael-24 <parishk1024@gmail.com>
2023-10-20 13:37:47 +0530

    simple refactor

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { errorHandler } from "../utils/error.js";
-import { env } from "node:process";
+
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashPassword = bcryptjs.hashSync(password, 15);
