8060cc604b58e8c27426fc2b39407321be88f6d6
Samael-24 <parishk1024@gmail.com>
2023-10-20 11:10:42 +0530

    Created Sign Up Api Route

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 // import dotenv from "dotenv";
 
 // dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 mongoose
   .connect(
@@ -19,6 +21,7 @@ mongoose
   });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(3000, () => {
   console.log("Server listening on Port 3000");
